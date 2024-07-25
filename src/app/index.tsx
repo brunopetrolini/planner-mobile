b2a5e9479815ea7a6b99630cf9f1e5b3e2c8d827
Bruno Petrolini <brunopetrolini@hotmail.com>
2024-07-25 15:24:27 -0300

    feat: uses tailwind

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -2,11 +2,11 @@ import { StatusBar, Text, View } from 'react-native';
 
 export default function Index() {
   return (
-    <View>
-      <Text>Hello World!</Text>
-      <Text>This is a React Native app</Text>
+    <View className="flex-1 justify-center items-center bg-zinc-950">
+      <Text className="text-zinc-50 text-4xl">Hello World!</Text>
+      <Text className="text-zinc-400 text-2xl">This is a React Native app</Text>
 
-      <StatusBar barStyle="dark-content" translucent />
+      <StatusBar barStyle="light-content" translucent />
     </View>
   );
 }
